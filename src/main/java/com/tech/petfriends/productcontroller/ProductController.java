f55b8a4fd2d5e55ad1e9b30f5c7deff9d2091a55
seok <143883309+dpoowa@users.noreply.github.com>
2024-10-14 15:46:18 +0900

    Update ProductController.java
    RquestMapping 주석 수정

--- src/main/java/com/tech/petfriends/productcontroller/ProductController.java
+++ src/main/java/com/tech/petfriends/productcontroller/ProductController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/product")
+@RequestMapping("/product")
 public class ProductController {
 
 	@GetMapping("/productlist")
