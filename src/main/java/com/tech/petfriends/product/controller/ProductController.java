c511f9dafd08fa593c225cff3e1aa1fbe49c6717
yooninkwon <yooninkwon@naver.com>
2024-10-15 17:21:06 +0900

    product 컨트롤러 내용 수정

--- src/main/java/com/tech/petfriends/product/controller/ProductController.java
+++ src/main/java/com/tech/petfriends/product/controller/ProductController.java
@@ -1,4 +1,4 @@
-package com.tech.petfriends.productcontroller;
+package com.tech.petfriends.product.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
