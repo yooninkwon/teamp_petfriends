66c58e205df4612445d31c32d44094f42239ac0d
seok <143883309+dpoowa@users.noreply.github.com>
2024-10-18 15:49:36 +0900

    Update MemberMapper.java

--- src/main/java/com/tech/petfriends/login/mapper/MemberMapper.java
+++ src/main/java/com/tech/petfriends/login/mapper/MemberMapper.java
@@ -6,7 +6,7 @@ import com.tech.petfriends.login.dto.MemberLoginDto;
 
 @Mapper
 public interface MemberMapper {
-    // 이메일과 비밀번호로 사용자 조회 (닉네임 포함)
+    // 이메일과 사용자 조회
     MemberLoginDto getMemberByEmail(String email);
     // 회원가입
     void insertMember(MemberLoginDto member);;
