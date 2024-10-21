c4cf653ff0e1b373352e59d3ee71e931b9b94271
yooninkwon <yooninkwon@naver.com>
2024-10-21 18:22:26 +0900

    productdetail -상품상세페이지 작업중 2 >상품정보, 이미지, 옵션 연결 완료

--- src/main/java/com/tech/petfriends/product/service/ProductDetailService.java
+++ src/main/java/com/tech/petfriends/product/service/ProductDetailService.java
@@ -1,9 +1,13 @@
 package com.tech.petfriends.product.service;
 
+import java.util.ArrayList;
+
 import org.springframework.ui.Model;
 
 import com.tech.petfriends.product.dao.ProductDao;
 import com.tech.petfriends.product.dto.ProductDetailDto;
+import com.tech.petfriends.product.dto.ProductDetailOptionDto;
+import com.tech.petfriends.product.dto.ProductDetailReviewRankDto;
 
 public class ProductDetailService implements ProductService {
 
@@ -22,14 +26,19 @@ public class ProductDetailService implements ProductService {
 
 		String pro_code = (String) model.getAttribute("productCode");
 		
-		model.addAttribute("product",productDetail(pro_code));
+		//제품상세페이지 _ 제품정보, 대표이미지 및 상세이미지
+		ProductDetailDto product = productDao.productDetail(pro_code);
+		model.addAttribute("product",product);
+		
+		//제품상세페이지 _ 리뷰평균점수, 리뷰갯수, 1/2/3/4/5점 갯수
+		ProductDetailReviewRankDto reviewRank = productDao.productReviewRank(pro_code);
+		model.addAttribute("reviewRank",reviewRank);
+		
+		//제품상세페이지 _ 제품 옵션 선택지 데이터
+		ArrayList<ProductDetailOptionDto> productOption = productDao.productOption(pro_code); 
+		model.addAttribute("productOption",productOption.get(0));
+		model.addAttribute("productOptionList",productOption);
 	}
 
-	//제품상세페이지 _ 제품정보, 대표이미지 및 상세이미지
-	public ProductDetailDto productDetail(String pro_code) {
-		ProductDetailDto product = productDao.productDetail(pro_code);
-		
-		return product;
-	}
 	
 }
