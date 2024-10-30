15667a090486e9fe3a68a42893f2902734ee4037
yooninkwon <yooninkwon@naver.com>
2024-10-31 00:38:00 +0900

    adminproduct 3 -상품 상세보기 데이터 불러오기 작업완료
    >편집(수정)기능 ajax 데이터 보내기 작업예정

---
+++ src/main/java/com/tech/petfriends/admin/dto/ProductDetailOptDto.java
@@ -0,0 +1,12 @@
+package com.tech.petfriends.admin.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDetailOptDto {
+	private String proopt_name;
+	private int proopt_price;
+	private int proopt_stock;
+}
